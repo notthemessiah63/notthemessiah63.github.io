34b12612bc49333af98c73056f4bbf2fa7a71e7c
Brian Newton <notthemessiah63@gmail.com>
2015-05-12 19:48:21 +0100

    return a constant back to calling id

--- scripts/ticTacToe.js
+++ scripts/ticTacToe.js
@@ -1,18 +1,10 @@
-var board = ['w','w','w','w','w','w','w','w','w'];
+var board = ['','','','','','','','',''];
 var lastPlayer = 'o';
 var winC1 = '';var winC2 = ''; var winC3 = ''; 
 var winR1 = ''; var winR2 = ''; var winR3 = ''; 
 var winD1 = ''; var winD2 = ''; 
 // define all combinations of win lines 
 var winLines = {
-  // '1': [board[0], board[1], board[2]],
-  // '2': [board[3], board[4], board[5]],
-  // '3': [board[6], board[7], board[8]],
-  // '4': [board[0], board[3], board[6]],
-  // '5': [board[1], board[4], board[7]],
-  // '6': [board[2], board[5], board[8]],
-  // '7': [board[0], board[4], board[8]],
-  // '8': [board[2], board[4], board[6]]
 };
 
 function dataSets() {
@@ -28,6 +20,7 @@ function dataSets() {
 $(document).ready(function(){
   $('.row').on('click', function(){
     var playmv = Number(this.id);
+    $(this).html("x");
     currPlayer = lastPlayer === 'x' ? "o" : "x";
     board[playmv] = currPlayer;
     lastPlayer = currPlayer;
@@ -35,12 +28,10 @@ $(document).ready(function(){
     for (i = 0; i < 8; i++) {
       switch (winLines[i]) {
         case "xxx":
-          console.log("The winner is 'x'");
-          // i = 8
+           i = 8
           break;
         case "ooo":
-          console.log("The winner is 'o'");
-          // i = 8
+           i = 8
           break;
         default:
       }
